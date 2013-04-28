5cf9c591f0ac262f8b227b2e6f98dd80963b317a
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2013-04-28 09:34:50 +0100

    Add configuration module to toggle features

---
+++ Team 1/HiveSenseTeam1/Model/Model/Configuration.cs
@@ -0,0 +1,65 @@
+using System;
+using Microsoft.SPOT;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+namespace HiveSenseTeam1.Model
+{
+    public class Configuration
+    {
+        char[] newLine = new[] { '\r', '\n' };
+
+        public Configuration(string configValues)
+        {
+            Values = Initialise(configValues);
+        }
+
+        public Configuration()
+        {
+            Values = new ArrayList();
+        }
+
+        public ArrayList Initialise(string configValues)
+        {
+            ArrayList values = new ArrayList();
+            foreach (var configLine in configValues.Split(newLine))
+            {
+                if (configLine == null || configLine == string.Empty)
+                {
+                    continue;
+                }
+                values.Add(ParseLine(configLine));
+            }
+            return values;
+        }
+
+        private DictionaryEntry ParseLine(string configLine)
+        {
+            var kvp = configLine.Split('=');
+            return new DictionaryEntry(kvp[0], kvp[1]);
+        }
+
+        public ArrayList Values
+        {
+            get;
+            private set;
+        }
+
+        public int this[string key]
+        {
+            get
+            {
+                foreach (var obj in Values)
+                {
+                    var kvp = (DictionaryEntry)obj;
+                    if ((string)kvp.Key == key)
+                    {
+                        return (int)kvp.Value;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
