6ea3c4094047600165566e8269054dc3506d2721
Mark Vallins <github@arbitrarystyle.com>
2013-04-28 13:38:32 +0100

    DisplayLogger pads all output to 16 characters

--- Team 1/HiveSenseTeam1/HiveSenseTeam1/Loggers/DisplayLogger.cs
+++ Team 1/HiveSenseTeam1/HiveSenseTeam1/Loggers/DisplayLogger.cs
@@ -7,10 +7,12 @@ namespace HiveSenseTeam1.Loggers
     {
         private Gadgeteer.Modules.GHIElectronics.Display_HD44780 charDisplay_;
         private bool line = false;
+        private const int DIGIT_COUNT = 5;
 
         public DisplayLogger(Gadgeteer.Modules.GHIElectronics.Display_HD44780 charDisplay)
         {
             charDisplay_ = charDisplay;
+            charDisplay_.Clear();
         }
 
         public void OnLogItem(Model.Measurement measurement)
@@ -23,7 +25,19 @@ namespace HiveSenseTeam1.Loggers
             {
                 charDisplay_.SetCursor(1, 0);
             }
-            charDisplay_.PrintString(measurement.Key + ": " + measurement.Value.ToString().Substring(0,5));
+
+            string dataString = measurement.Key + ": " + measurement.Value.ToString().Substring(0, DIGIT_COUNT);
+
+            // Padd the data string with spaces to the length of the character
+            // display, 16 characters. This will wipe out any rogue characters
+            // from previous longer strings on that line.
+            if (dataString.Length < 16)
+            {
+                string emptyString = new string(' ', 16 - dataString.Length);
+                dataString += emptyString;
+            }
+            charDisplay_.PrintString(dataString);
+
             line = !line;
         }
 
